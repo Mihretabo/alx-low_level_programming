ffd38d9c52970fc3562a633bb36c86dd43dca395
Mihretabo <mehareniabo@gmail.com>
2022-06-30 09:43:27 -0700

    nvdvd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(void)
+int main(void))
 {
-	printf("Size of a char : %d (s)\n" byte(s))
-		printf("Size of an int : %d (s)\n" byte(s))
-		printf("Size of a long int : %d (s)\n" byte(s))
-		printf("Size of a long long int : %d (s)\n" byte(s))
-		printf("Size of ai float : %d (s)\n" byte(s))
+	char c;
+	int i;
+	long int li:
+	long long int lli;
+	float fl;
+
+	printf("Size of a char; %lu byte(s)\n", sizeof(c))
+	printf("Size of an int; %lu byte(s)\n", sizeof(i))
+	printf("Size of a long int; %lu byte(s)\n", sizeof(li))
+	printf("Size of a long long int; %lu byte(s)\n", sizeof(lli))
+	printf("Size of ai float; %lu byte(s)\n", sizeof(fl))
+	return (0)
 }
