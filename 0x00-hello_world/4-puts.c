d781ab0f84d06ee434363d186f1cdb8a4b9d574a
Mihretabo <mehareniabo@gmail.com>
2022-06-30 07:56:08 -0700

    hjkfhksadf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-
 int main(void)
 {
-puts(*\ *Programming is like building a multilingual puzzleReturn);
-return (0);
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Size of a char : %d (s)\n" byte(s))
+		printf("Size of an int : %d (s)\n" byte(s))
+		printf("Size of a long int : %d (s)\n" byte(s))
+		printf("Size of a long long int : %d (s)\n" byte(s))
+		printf("Size of ai float : %d (s)\n" byte(s))
+}
