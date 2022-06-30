71cb6b11a1454da22e94ba119bcd2c96fc509edd
Mihretabo <mehareniabo@gmail.com>
2022-06-30 10:18:59 -0700

    bjcsjc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void))
 	long long int lli;
 	float fl;
 
-	printf("Size of a char; %lu byte(s)\n", sizeof(c))
-	printf("Size of an int; %lu byte(s)\n", sizeof(i))
-	printf("Size of a long int; %lu byte(s)\n", sizeof(li))
-	printf("Size of a long long int; %lu byte(s)\n", sizeof(lli))
-	printf("Size of ai float; %lu byte(s)\n", sizeof(fl))
-	return (0i)
+	printf("Size of a char; %lu byte(s)\n", sizeof(char));
+	printf("Size of an int; %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int; %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int; %lu byte(s)\n", sizeof(long long int));
+	printf("Size of ai float; %lu byte(s)\n", sizeof(float));
+	return (0);
 }
