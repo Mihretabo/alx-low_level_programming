26c4d6c651dde215005a042efc2771386981700e
Mihretabo <mehareniabo@gmail.com>
2022-07-14 04:30:43 -0700

    jkjk

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * * print_number- prints an integer.
- * * @n: input integer.
- * * Return: no return.
+ * * main - check the code for Holberton School students.
+ * *
+ * * Return: Always 0.
  */
 
 int main(void)
@@ -21,7 +21,7 @@ int main(void)
 	 * * - only one statement
 	 * * - you are not allowed to code anything else than this line of code
 	 */
-	p[5] = 98;
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
