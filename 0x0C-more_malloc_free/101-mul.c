3857921e35050d7b9a080b84641e52c30925e1b3
Mihretabo <mehareniabo@gmail.com>
2022-07-28 08:08:42 -0700

    updated

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,9 +5,9 @@
 
 /**
  * _is_zero - determines if any number is zero
- * @argv: argument vector.
- *
- * Return: no return.
+ * * @argv: argument vector.
+ * *
+ * * Return: no return.
  */
 void _is_zero(char *argv[])
 {
@@ -35,10 +35,11 @@ void _is_zero(char *argv[])
 }
 
 /**
- *_initialize_array - set memery to zero in a new array
- * @ar: char array.
- * @lar: length of the char array.
- * Return: pointer of a char array.
+ * _initialize_array - set memery to zero in a new array
+ * * @ar: char array.
+ * * @lar: length of the char array.
+ * *
+ * * Return: pointer of a char array.
  */
 char *_initialize_array(char *ar, int lar)
 {
@@ -52,11 +53,11 @@ char *_initialize_array(char *ar, int lar)
 
 /**
  * _checknum - determines length of the number
- * and checks if number is in base 10.
- * @argv: arguments vector.
- * @n: row of the array.
- *
- * Return: length of the number.
+ * * and checks if number is in base 10.
+ * * @argv: arguments vector.
+ * * @n: row of the array.
+ * *
+ * * Return: length of the number.
  */
 int _checknum(char *argv[], int n)
 {
@@ -74,11 +75,11 @@ int _checknum(char *argv[], int n)
 
 /**
  * main - Entry point.
- * program that multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: arguments vector.
- *
- * Return: 0 - success.
+ * * program that multiplies two positive numbers.
+ * * @argc: number of arguments.
+ * * @argv: arguments vector.
+ * *
+ * * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
