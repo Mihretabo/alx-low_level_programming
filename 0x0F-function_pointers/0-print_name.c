b11c0e565d774e1f6d15d9be90d1953922280699
Mihretabo <mehareniabo@gmail.com>
2022-08-03 09:44:49 -0700

    function pointer update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,13 @@
 
 /**
  * print_name - Print a name
- * * @name:name
- * * @f:ponter to function
- * * Return: no return
+ * @name:name
+ * @f:ponter to function
+ * Return: no return
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (f != NULL)
-f(name);
+	if (f != NULL)
+		f(name);
 }
