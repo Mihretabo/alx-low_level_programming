771a5c419c39310bd5c0d77700821e513e5af871
Mihretabo <mehareniabo@gmail.com>
2022-08-04 09:16:08 -0700

    variadic updated

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,32 +9,30 @@
  */
 void print_c(va_list c)
 {
-	printf("%c", va_arg(c, int));
+printf("%c", va_arg(c, int));
 }
 
 /**
  * print_s - prints a string
  * @s: string to print
- *
  * Return: void
  */
 void print_s(va_list s)
 {
-	char *str = va_arg(s, char *);
-
-	if (str == NULL)
-		str = "(nil)";
-	printf("%s", str);
+char *str = va_arg(s, char *);
+if (str == NULL)
+str = "(nil)";
+printf("%s", str);
 }
 
 /**
  * print_i - prints an int
  * @i: int to print
- * Return: void 
+ * Return: void
  */
 void print_i(va_list i)
 {
-	printf("%d", va_arg(i, int));
+printf("%d", va_arg(i, int));
 }
 
 /**
@@ -44,7 +42,7 @@ void print_i(va_list i)
  */
 void print_f(va_list f)
 {
-	printf("%f", va_arg(f, double));
+printf("%f", va_arg(f, double));
 }
 
 /**
@@ -54,35 +52,35 @@ void print_f(va_list f)
  */
 void print_all(const char * const format, ...)
 {
-	unsigned int i, j;
-	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"f", print_f},
-		{NULL, NULL}
-	};
-	va_list valist;
-	char *separator = "";
+unsigned int i, j;
+print_t p[] = {
+{"c", print_c},
+{"s", print_s},
+{"i", print_i},
+{"f", print_f},
+{NULL, NULL}
+};
+va_list valist;
+char *separator = "";
 
-	va_start(valist, format);
-	i = 0;
-	while (format && format[i])
-	{
-		j = 0;
-		while (p[j].t != NULL)
-		{
-			if (*(p[j].t) == format[i])
-			{
-				printf("%s", separator);
-				p[j].f(valist);
-				separator = ", ";
-				break;
-			}
-			j++;
-		}
-		i++;
-	}
-	va_end(valist);
-	printf("\n");
+va_start(valist, format);
+i = 0;
+while (format && format[i])
+{
+j = 0;
+while (p[j].t != NULL)
+{
+if (*(p[j].t) == format[i])
+{
+printf("%s", separator);
+p[j].f(valist);
+separator = ", ";
+break;
+}
+j++;
+}
+i++;
+}
+va_end(valist);
+printf("\n");
 }
